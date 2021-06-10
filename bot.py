2f8b08c7dbc6e72b441bfbe3d11446774717750a
soorajallu <80518137+soorajallu@users.noreply.github.com>
2021-06-10 11:44:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -62,7 +62,10 @@ def echo(client, message):
     InlinekeyboardButton("Russian",callback_dat='ru')
     ]
     
- message.reply_text("Select language 👇",reply_to_message_id = message.message_id, reply_markup = keybord)
+	 
+	
+	 
+message.reply_text("Select language 👇",reply_to_message_id = message.message_id, reply_markup = keybord)
     
     
 @app.on_callback_query()
